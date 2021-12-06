3aaf116eb4c101aa09400e0e91feb8eb9d3128cc
qhduan <mail@qhduan.com>
2021-12-06 18:27:54 +0800

    fix buuild scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p sr-build
-(cd sr-build && npx electron-packager ../ --asar --platform=darwin --overwrite --ignore="\.git(ignore|modules)|sr-build")
-(cd sr-build && npx electron-packager ../ --asar --platform=win32 --overwrite --ignore="\.git(ignore|modules)|sr-build")
+(cd sr-build && npx electron-packager ../ --platform=darwin --arch=arm64 --overwrite --ignore="\.git(ignore|modules)|sr-build")
+(cd sr-build && npx electron-packager ../ --platform=darwin --overwrite --ignore="\.git(ignore|modules)|sr-build")
+(cd sr-build && npx electron-packager ../ --platform=win32 --overwrite --ignore="\.git(ignore|modules)|sr-build")
 
