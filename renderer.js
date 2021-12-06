011b509316079e525c05ff0526143047f8aea5cc
qhduan <mail@qhduan.com>
2021-12-06 19:29:11 +0800

    try add button

--- renderer.js
+++ renderer.js
@@ -146,3 +146,12 @@ async function loadFile(event) {
         }
     })
 }
+
+document.querySelector('#homepage').addEventListener('click', event => {
+    event.preventDefault()
+    require("shell").openExternal('https://github.com/CSPHQ/electron-real-esrgan')
+})
+document.querySelector('#license').addEventListener('click', event => {
+    event.preventDefault()
+    require("shell").openExternal('https://github.com/CSPHQ/electron-real-esrgan/blob/master/LICENSE')
+})
--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir -p sr-build-zip
+rm -rf sr-build-zip/*
 cd sr-build
-for x in ./*
+for x in *
 do
     echo ${x}
-    zip -rq --symlinks ../sr-build-zip/${x}.zip ./${x}
+    # npm i electron-installer-zip -g
+    electron-installer-zip ${x} ../sr-build-zip/${x}.zip
+    # zip -rq --symlinks ../sr-build-zip/${x}.zip ./${x}
 done
